aa5f2a0e9bf6b5da6b4c0402ad9d1f6898e5eeb8
Kyron Longwood <kyronlongwood@gmail.com>
2021-04-24 20:05:33 -0400

    finally able to list groceries and merchandise

--- NoteTaker.py
+++ NoteTaker.py
@@ -1,13 +1,44 @@
 import gkeepapi
 import keyring
 import json
-token = kee
-with open("cred.json",) as cred:
-    credentials = json.load(cred)
-keep = gkeepapi.Keep()
-keep.login(credentials["email"], credentials["password"])
+import getpass
 
-note = gkeepapi.createNote('Todo', 'Eat breakfast')
+#token = kee
+#with open("cred.json",) as cred:
+#credentials = json.load(cred)
+config = dict()
+config["username"] =#input("email: ")
+config["password"] = #getpass.getpass("password: ")
+keep = gkeepapi.Keep()
+example = keep.login(config["username"], config["password"])
+token = keep.getMasterToken()
+#keyring.set_password("google-keep-token",username, token)
+""" 
+logged_in = False
+if not logged_in and token:
+    logger.info("Authenticating with token")
+    state = util.load(args.config_dir, config["username"])
+
+    try:
+        keep.resume(config["username"], token, state=state, sync=False)
+        logged_in = True
+        logger.info("Success")
+    except gkeepapi.exception.LoginException:
+        print(8)
+
+
+if logged_in:
+    password = getpass.getpass()
+    try:
+        keep.login(config["username"], password, sync=False)
+        logged_in = True
+        del password
+        token = keep.getMasterToken()
+        keyring.set_password("google-keep-token", config["username"], token)
+        #logger.info("Success")
+    except gkeepapi.exception.LoginException:
+        pass """
+note = keep.createNote('Todo', 'Eat breakfast')
 note.pinned = True
 note.color = gkeepapi.node.ColorValue.Red
 
@@ -15,3 +46,6 @@ keep.sync()
 
 print(note.title)
 print(note.text)
+
+Merchandise = keep.get("1R4lilvk5kmHpNIMqQG5Go3FYnWr5Gk_mslkJ2IzamZpnl6W8_mW4IBdsqZnW3OIvpl7Q")
+Groceries = keep.get("1Y_H6kCujK2r0NfhBK7tlQGPON8iMXqW3Pi7ot4BB0jT2vz0Ti_SGhdZIm2QBPTxKw705RWK2Mg")
