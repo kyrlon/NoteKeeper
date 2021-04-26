b3c63b27d7080d13e5898b21952d1213b2578ac1
Kyron Longwood <kyronlongwood@gmail.com>
2021-04-25 23:23:50 -0400

    changed func for restoring from file

--- NoteTaker.py
+++ NoteTaker.py
@@ -1,49 +1,18 @@
 import gkeepapi
-import keyring
+#import keyring
 import json
 import getpass
 from collections import defaultdict
 
 with open("cred.json",) as cred:
     config = json.load(cred)
-#config = dict()
 keep = gkeepapi.Keep()
 example = keep.login(config["username"], config["password"])
 token = keep.getMasterToken()
-#keyring.set_password("google-keep-token",username, token)
-""" 
-logged_in = False
-if not logged_in and token:
-    logger.info("Authenticating with token")
-    state = util.load(args.config_dir, config["username"])
-
-    try:
-        keep.resume(config["username"], token, state=state, sync=False)
-        logged_in = True
-        logger.info("Success")
-    except gkeepapi.exception.LoginException:
-        print(8)
-
-
-if logged_in:
-    password = getpass.getpass()
-    try:
-        keep.login(config["username"], password, sync=False)
-        logged_in = True
-        del password
-        token = keep.getMasterToken()
-        keyring.set_password("google-keep-token", config["username"], token)
-        #logger.info("Success")
-    except gkeepapi.exception.LoginException:
-        pass """
-#note = keep.createNote('Todo', 'Eat breakfast')
-#note.pinned = True
-#note.color = gkeepapi.node.ColorValue.Red
 
 keep.sync()
 
-#print(note.title)
-#print(note.text)
+
 
 Merchandise = keep.get("1R4lilvk5kmHpNIMqQG5Go3FYnWr5Gk_mslkJ2IzamZpnl6W8_mW4IBdsqZnW3OIvpl7Q")
 Groceries = keep.get("1Y_H6kCujK2r0NfhBK7tlQGPON8iMXqW3Pi7ot4BB0jT2vz0Ti_SGhdZIm2QBPTxKw705RWK2Mg")
@@ -75,22 +44,34 @@ def fix_list(store_incremental_lists):
                     broken_dashes.remove(val.text)
                     break
         store_incremental_lists.add(new_entry, False)
-    #keep.sync()
+    keep.sync()
 
 def g_keep_backup():
     backup_dict = dict()
     for num,shopping_item in enumerate(Groceries.items):
         backup_dict[str(num)] = [shopping_item.checked, shopping_item.text]
-    with open("backup_.txt", "w") as file_t:
+    with open("backup_json.txt", "w") as file_t:
         file_t.write(json.dumps(backup_dict, sort_keys=True, indent=4))
     
     #Human Readable
-    with open("human_readable.txt", "w") as back_up:
-        back_up.write(Groceries.text)
+    with open("human_readable.txt", "w", encoding='utf-8') as f:
+        f.write(Groceries.text)
 
 def g_keep_restore():
-    pass
+    gnote = keep.createList('Title')
+    gnote.delete()
+    with open("backup_json.txt","r") as f:
+        data = json.load(f)
+    print(data)
+    gnote = keep.createList('Title')
+    for key, value in enumerate(data.items()):
+        _, food_item = value
+        gnote.add(food_item[1],food_item[0])
+    print(1)
+
 
 
 if __name__ == "__main__":
     fix_list(Groceries)
+    g_keep_backup()
+    g_keep_restore()
